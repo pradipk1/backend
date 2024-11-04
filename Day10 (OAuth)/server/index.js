a455d91495357876647a372b0f5e99c1ecc6b6e0
pradipk1 <pradipshukla12699@gmail.com>
2024-11-04 15:47:57 +0530

    updated!

--- Day10 (OAuth)/server/index.js
+++ Day10 (OAuth)/server/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const connectDatabase = require('./Database/connectDatabase');
+const connectDatabase = require('./database/connectDatabase');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan('tiny')); 
 
 // function logRequest(req, res, next) {
 //     console.log(new Date(), req.method, req.url);
